c54a031ffb6b553a1f7029326ac5c35c82462b2f
Thorsten Gunkel <tgunkel-lists@tgunkel.de>
2014-05-10 00:29:24 +0200

    - Fix estimation of heigh value for given point based on heigh of the corners

--- src/01_starting_easy/Tile.cpp
+++ src/01_starting_easy/Tile.cpp
@@ -36,23 +36,32 @@ double Tile::getEstimatedValue(const unsigned int x, const unsigned int y) const
   assert(y<=getLowerLeft().getPcpY());
 
   // calculate to what percentage your point is to the right or down side of this Tile
-  double rightInPercent=(x-getUpperLeft().getPcpX())/(double)(getUpperRight().getPcpX()-getUpperLeft().getPcpX());
-  //double downInPercent= (y-getUpperLeft().getPcpY())/(double)(getLowerLeft().getPcpY()-getUpperLeft().getPcpY());
-  double downInPercent= (y-getUpperLeft().getPcpY())/(double)(getUpperLeft().getPcpY()-getLowerLeft().getPcpY());
+  double rightInPercent=((double)x-(double)getUpperLeft().getPcpX())/((double)getUpperRight().getPcpX()-(double)getUpperLeft().getPcpX());
+  double downInPercent= ((double)y-(double)getUpperLeft().getPcpY())/((double)getLowerLeft().getPcpY()-(double)getUpperLeft().getPcpY());
 
   assert(rightInPercent>=0 && rightInPercent<=1);
   assert(downInPercent>=0  && downInPercent<=1);
 
-  // the more your point is to one corner, the more do we count that corner in
-  double lr_est=((double)getLowerRight().getHeight())*   rightInPercent *   downInPercent;
-  double ul_est=((double)getUpperLeft(). getHeight())*(1-rightInPercent)*(1-downInPercent);
-  double ur_est=((double)getUpperRight().getHeight())*   rightInPercent *(1-downInPercent);
-  double ll_est=((double)getLowerLeft() .getHeight())*(1-rightInPercent)*   downInPercent;
+  // find a fair x value between upper left and upper right
+  double upperRow=(1.0-rightInPercent)*(double)getUpperLeft().getHeight()+(rightInPercent)*(double)getUpperRight().getHeight();
 
-  // form average
-  double res=(lr_est+ul_est+ur_est+ll_est)/4.0;
+  // find a fair x value between lower left and lower right
+  double lowerRow=(1.0-rightInPercent)*(double)getLowerLeft().getHeight()+(rightInPercent)*(double)getLowerRight().getHeight();
 
-  // std::cout << "Estimated value: for " << x << "/" << y << " right: " << rightInPercent << " down: " << downInPercent << " res: " << res << std::endl;
+  /*
+  std::cout << "UPPER: " << getUpperLeft().getHeight() << ", " << getUpperRight().getHeight() << ": " << upperRow << std::endl;
+  std::cout << "LOWER: " << getLowerLeft().getHeight() << ", " << getLowerRight().getHeight() << ": " << lowerRow << std::endl;
+  */
+
+  assert(upperRow+0.1>=std::min((double)getUpperLeft().getHeight(),  (double)getUpperRight().getHeight()));
+  assert(upperRow-0.1<=std::max((double)getUpperLeft().getHeight(),  (double)getUpperRight().getHeight()));
+  assert(lowerRow+0.1>=std::min((double)getLowerLeft().getHeight(),  (double)getLowerRight().getHeight()));
+  assert(lowerRow-0.1<=std::max((double)getLowerLeft().getHeight(),  (double)getLowerRight().getHeight()));
+
+  // now find a fair y value between the both fair x values
+  double res=(1.0-downInPercent)*upperRow+(downInPercent)*lowerRow;
+
+  //std::cout << "Estimated value: for " << x << "/" << y << " right: " << rightInPercent << " down: " << downInPercent << " res: " << res << std::endl;
 
   return res;
 }
